fc884f9f6fc093ff2c54b88d92664ebb5c8c96f0
ToHendrix <tom-hendrix@live.nl>
2020-05-24 13:57:10 +0200

    Initial files for statemachine and activationmode

---
+++ ActivationMode.hpp
@@ -0,0 +1,24 @@
+/*
+ * ActivationMode.hpp
+ *
+ *  Created on: May 20, 2020
+ *      Author: tom-h
+ */
+#ifndef ACTIVATIONMODE_HPP_
+#define ACTIVATIONMODE_HPP_
+
+#include "StateMachine.h"
+#include "OBCTelemetryContainer.h"
+
+namespace activation{
+
+int BootCount;
+//True when the 30 mins have past
+bool TimerDone;
+//This function can be called from Statemachine
+//and is therefore the only one in the hpp
+void Activation();
+
+}
+
+#endif /* ACTIVATIONMODE_HPP_ */
