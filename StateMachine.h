1209bebe62f046b064dc16580f3f906b497338b5
Zhuoheng Li <33491471+StarCycle@users.noreply.github.com>
2020-07-02 14:07:53 +0200

    Update StateMachine.h

--- StateMachine.h
+++ StateMachine.h
@@ -8,11 +8,7 @@
 #ifndef STATEMACHINE_H_
 #define STATEMACHINE_H_
 
-<<<<<<< HEAD
-#include <OBCVariableContainer.h>
-=======
 #include "OBCDataContainer.h"
->>>>>>> 336b15269de0100ea682facb34b021a7d6e13c93
 
 void StateMachineInit();
 void StateMachine();
